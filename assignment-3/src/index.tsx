47d480ce81059e1441398bdd6dd04fb3468bcdaa
TrungKien2705 <nt.kien.2705@gmail.com>
2023-09-29 00:34:21 +0700

    update assignment-3

--- assignment-3/src/index.tsx
+++ assignment-3/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import {ThemeProvider} from "./hook/useTheme";
 
 const rootElement = document.getElementById('root')
 
@@ -10,7 +11,9 @@ if (rootElement) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <React.StrictMode>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </React.StrictMode>,
   )
   reportWebVitals();
